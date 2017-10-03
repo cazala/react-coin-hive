3e4022e525a53f71325edc8f71bab9d16934ee28
Florent Béjina <florent@bejina.fr>
2017-10-03 16:24:07 +0200

    update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,15 +4,44 @@ import { render } from 'react-dom'
 import CoinHive from '../../src'
 
 class Demo extends Component {
+  state = {
+    autoThreads: true,
+  }
+
+  _handleEnableAutoThreads = () => {
+    this.setState({ autoThreads: true })
+  }
+
+  _handleDisableAutoThreads = () => {
+    this.setState({ autoThreads: false })
+  }
 
   render() {
     return <div>
       <h1>react-coin-hive Demo</h1>
+      <div>
+        autoThreads :&nbsp;
+        <input
+          type="button"
+          value="on"
+          disabled={this.state.autoThreads}
+          onClick={this._handleEnableAutoThreads}
+        />
+        <input
+          type="button"
+          value="off"
+          disabled={!this.state.autoThreads}
+          onClick={this._handleDisableAutoThreads}
+        />
+      </div>
       <CoinHive
-        siteKey='NjBUIBfgmgqwSRGjemP5JQCNFJu5UJTx'
-        onInit={miner => setInterval(() => console.log(miner.getHashesPerSecond()), 1000)}
+        autoThreads={this.state.autoThreads}
+        onInit={miner => setInterval(() => CoinHive.displayMiner(miner), 1000)}
         onStart={() => console.log('started')}
         onStop={() => console.log('stopped')}
+        siteKey='NjBUIBfgmgqwSRGjemP5JQCNFJu5UJTx'
+        startOnIdle
+        timeout={1000}
       />
     </div>
   }
